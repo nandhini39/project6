c4202de4a310652eb1ca783e443c2216236b90d4
nandhini <nandhini@gmail.com>
2024-10-04 10:56:19 +0530

    Addition.java with content nandhini

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Addition 
+{
+	public static viod main(String args[])
+	{
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+	}
+}
---
+++ addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+	public static void (String args[])
+	{
+		a=10,b=20,c;
+			c=a+b;
+		System.out.println(c);
+	}
+}
+     
